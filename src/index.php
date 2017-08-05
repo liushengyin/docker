8cba5255e9820ac774169a926ab6c4b6a1851b6d
刘圣印 <shengyinliu@outlook.com>
2017-08-05 21:46:19 +0800

    ADD || php+apache+mysql

--- src/index.php
+++ src/index.php
@@ -1,2 +1,22 @@
 <?php
 echo "hello world";
+$mysql_conf = array(
+    'host'    => 'mysql', 
+    'db'      => 'app', 
+    'db_user' => 'root', 
+    'db_pwd'  => 'root', 
+    );
+
+$mysqli = new mysqli($mysql_conf['host'], $mysql_conf['db_user'], $mysql_conf['db_pwd'], $mysql_conf['db']);
+
+if(mysqli_connect_errno())
+{
+    echo mysqli_connect_error();
+}
+
+//结果集示例
+$sql = "show databases";
+$result = $mysqli->query($sql);
+$databases = $result->fetch_all(MYSQLI_ASSOC);
+var_dump($databases);
+$mysqli->close();
